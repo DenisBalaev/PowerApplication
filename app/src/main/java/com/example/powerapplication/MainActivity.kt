2ec14948c912a2e4d20dff1b113120a09b3e4670
Denis <balaev@mail.ru>
2023-10-25 14:53:40 +0300

    Вроде как рабочая

--- app/src/main/java/com/example/powerapplication/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.powerapplication
 
-import android.content.ComponentName
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication.databinding.ActivityMainBinding
@@ -13,6 +16,21 @@ import com.example.powerapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     val binding by viewBinding(ActivityMainBinding::bind)
+    val REQUEST_OVERLAY_PERMISSION = 1
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private val requestOverlayPermission = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (Settings.canDrawOverlays(this)) {
+
+        } else {
+
+        }
+
+        finish()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +45,22 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 "com.miui.permcenter.autostart.AutoStartManagementActivity"
             )
             startActivity(intent)
+        }*/
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION)
+        }
+        /*val callHomeSettingIntent = Intent(Settings.ACTION_HOME_SETTINGS)
+        startActivity(callHomeSettingIntent)*/
+
+        /*if (!Settings.canDrawOverlays(this@MainActivity)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            intent.data = Uri.parse("package:$packageName")
+            requestOverlayPermission.launch(intent)
         }*/
     }
 }
