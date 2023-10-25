7a72b7fea78f8282ae081f8556b92ccc241cdc92
Denis <balaev@mail.ru>
2023-10-25 11:50:33 +0300

    Точка 2

--- app/src/main/java/com/example/powerapplication/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.powerapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -14,5 +17,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("tagPower","MainActivity")
+
+        /*val manufacturer = "xiaomi"
+        if (manufacturer.equals(Build.MANUFACTURER, ignoreCase = true)) {
+            //this will open auto start screen where user can enable permission for your app
+            val intent = Intent()
+            intent.component = ComponentName(
+                "com.miui.securitycenter",
+                "com.miui.permcenter.autostart.AutoStartManagementActivity"
+            )
+            startActivity(intent)
+        }*/
     }
 }
