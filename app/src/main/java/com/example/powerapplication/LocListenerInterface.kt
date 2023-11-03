d06c40da6cf8458e1c05c49a94c77ef2881ab7d1
Denis <balaev@mail.ru>
2023-11-03 12:09:26 +0300

    Фексирование готового

--- app/src/main/java/com/example/powerapplication/LocListenerInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.example.powerapplication
-
-import android.location.Location
-
-interface LocListenerInterface {
-    fun onLocationChange(location: Location)
-}
--- app/src/main/java/com/example/powerapplication/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.powerapplication
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -13,20 +12,21 @@ import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main),LocListenerInterface {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     val binding by viewBinding(ActivityMainBinding::bind)
     val REQUEST_OVERLAY_PERMISSION = 1
+    var fusedClient: FusedLocationProviderClient? = null
 
-    val PERMISSIONS_REQUEST_LOCATION = 777
-    private var locationManager: LocationManager? = null
-    private lateinit var myLocListener:MyLocListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +35,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),LocListenerInterf
     }
 
     private fun init(){
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        myLocListener = MyLocListener()
-        myLocListener.setLocListenerInterface(this@MainActivity)
-
         chekPermissionGPS()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Settings.canDrawOverlays(this)) {
@@ -50,40 +46,46 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),LocListenerInterf
         }
     }
 
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        //grand()
+
+        if(permissions.entries.toList().all { it.value }){
+            grand()
+        }else{
+            chekPermissionGPS()
+        }
+
+    }
+
+    private fun grand(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedClient!!.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val loc = location.provider + " ${formatLocation(location)}"
+                    binding.tvLocationGPS.text = "${binding.tvLocationGPS.text} $loc"
+                }
+            }
+
+        }else {
+            chekPermissionGPS()
+        }
+    }
+
     private fun chekPermissionGPS(){
-        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION),
-                PERMISSIONS_REQUEST_LOCATION
-            )
-        }else{
-            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER,2L,1f,myLocListener)
-        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CAMERA
+        ))
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSIONS_REQUEST_LOCATION && grantResults[0] == RESULT_OK){
-            chekPermissionGPS()
-        }else{
-            chekPermissionGPS()
-        }
-    }
-
-    private fun showLocation(location: Location?) {
-        if (location == null) return
-        if (location.provider == LocationManager.GPS_PROVIDER) {
-            binding.tvLocationGPS.text = formatLocation(location)
-        }
-    }
 
     @SuppressLint("DefaultLocale")
-    private fun formatLocation(location: Location?): String? {
-        return if (location == null) "" else "${location.latitude} ${location.longitude}"
-    }
-
-    override fun onLocationChange(location: Location) {
-        showLocation(location)
+    private fun formatLocation(location: Location?): String {
+        return if (location == null) "" else "${location.latitude}, ${location.longitude}"
     }
 }
