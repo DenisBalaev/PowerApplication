699fbbde4839419978efede5f23cf27db85c1bf7
Denis <balaev@mail.ru>
2023-11-07 22:02:09 +0300

    Фиксирование

--- app/src/main/java/com/example/powerapplication/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication/MainActivity.kt
@@ -17,8 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication.databinding.ActivityMainBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -27,6 +26,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     val REQUEST_OVERLAY_PERMISSION = 1
     var fusedClient: FusedLocationProviderClient? = null
 
+    private var mRequest: LocationRequest? = null
+    private var mCallback: LocationCallback? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,13 +63,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private fun grand(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedClient = LocationServices.getFusedLocationProviderClient(this)
+            /*fusedClient = LocationServices.getFusedLocationProviderClient(this)
             fusedClient!!.lastLocation.addOnSuccessListener { location ->
                 if (location != null) {
                     val loc = location.provider + " ${formatLocation(location)}"
-                    binding.tvLocationGPS.text = "${binding.tvLocationGPS.text} $loc"
+                    binding.tvLocationGPS.text = "${binding.tvLocationGPS.text} $loc \n"
                 }
-            }
+            }*/
+            callbackGPS()
+            fusedClient = LocationServices.getFusedLocationProviderClient(this)
+            startLocationUpdates()
 
         }else {
             chekPermissionGPS()
@@ -83,6 +87,56 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         ))
     }
 
+    private fun callbackGPS(){
+        mCallback = object : LocationCallback() {
+            //This callback is where we get "streaming" location updates. We can check things like accuracy to determine whether
+            //this latest update should replace our previous estimate.
+            override fun onLocationResult(locationResult: LocationResult?) {
+                if (locationResult == null) {
+                    return
+                }
+                for (loc in locationResult.locations) {
+                    val locs = "1)" + "${formatLocation(loc)}"
+                    binding.tvLocationGPS.text = "${binding.tvLocationGPS.text} $locs \n"
+                }
+
+                val loc = "2)" + formatLocation(locationResult.lastLocation)
+                binding.tvLocationGPS.text = "${binding.tvLocationGPS.text} $loc \n"
+            }
+
+            override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+                super.onLocationAvailability(locationAvailability)
+            }
+        }
+
+        mRequest = LocationRequest.create().apply {
+            interval = 5000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        } /*LocationRequest().apply {
+            interval = 5000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }*/
+    }
+
+    private fun createLocationRequest() {
+        val mLocationRequest = LocationRequest.create().apply {
+            interval = 5000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedClient?.removeLocationUpdates(mCallback)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedClient?.requestLocationUpdates(mRequest, mCallback, null)
+    }
 
     @SuppressLint("DefaultLocale")
     private fun formatLocation(location: Location?): String {
